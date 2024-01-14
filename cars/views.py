a0df5bdfe272c6996d45acb9d82cbe01d27f56d6
jitbherwaniofficial <jitbherwani92@gmail.com>
2024-01-14 14:49:35 +0530

    Cars Features added

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Car
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_date')
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, id=id)
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars/car_detail.html', data)
